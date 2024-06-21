237c9099bf7414848d00575a0976c6138d02999a
htw07 <106394825+htw07@users.noreply.github.com>
2024-06-21 09:11:58 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -26,9 +26,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 #  T E S T   C A S E S
 ######################################################################
 
+
 class TestAccountService(TestCase):
-
-
     """Account Service Tests"""
 
     @classmethod
